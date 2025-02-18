dff956b81433e3240e10899d23b94d640aff85a1
shubhamyadav3100 <shubhamyadav32100@gmail.com>
2025-02-18 15:57:53 +0530

    initial project setup with Gradle and Spring Boot

---
+++ src/main/java/com/bookingserive/UberBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookingserive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberBookingServiceApplication.class, args);
+	}
+
+}
